ca5b104e3ae110afc5cc3bb614fae252532c8f3a
Daniel Pokladek <danielpokladek@gmail.com>
2016-10-28 11:07:03 +0100

    -
    -

---
+++ list - danielpokladek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list___danielpokladek
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            LinkedList list = new LinkedList();
+            list.InsertBeginning(32);
+            list.InsertBeginning(64);
+            list.InsertBeginning(128);
+            list.InsertBeginning(256);
+        }
+    }
+}
